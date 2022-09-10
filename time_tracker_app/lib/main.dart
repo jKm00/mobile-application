a4052e7fc1d7b9a1b9edb378ae712f4028930238
jKm00 <joakied@stud.ntnu.no>
2022-09-10 16:02:31 +0200

    Completed firebase initialization

--- time_tracker_app/lib/main.dart
+++ time_tracker_app/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_app/app/sign_in/sign_in_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
